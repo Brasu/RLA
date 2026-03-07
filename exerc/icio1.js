1e625b40e73ed69b86f0085749e7b32140fd831c
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-07 20:36:02 -0300

    exercicio1.js

---
+++ exerc/icio1.js
@@ -0,0 +1,8 @@
+let idade = parseInt(prompt("Qual sua idade? "));
+let tempoContribuicao = parseFloat(prompt("Qual seu tempo de contribuição? "));
+
+if (idade >= 65 || tempoContribuicao >= 30 && idade >= 60) {
+    console.log("Parabéns, você já pode se aposentar!")
+} else {
+    console.log("Você ainda não pode se aposentar.")
+}
