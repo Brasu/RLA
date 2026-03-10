0e099dc179186334de585d7df7ba2e4b84cce4f1
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-10 17:05:05 -0300

    Atualizar o exercicio-1.js

--- exercicio-1.js
+++ exercicio-1.js
@@ -2,7 +2,7 @@ let idade = parseInt(prompt("Qual sua idade? "));
 let tempoContribuicao = parseFloat(prompt("Qual seu tempo de contribuição? "));
 
 if (idade >= 65 || tempoContribuicao >= 30 && idade >= 60) {
-    console.log("Parabéns, você já pode se aposentar!")
+    console.log("Parabéns, você já pode se aposentar!");
 } else {
-    console.log("Você ainda não pode se aposentar.")
+    console.log("Você ainda não pode se aposentar.");
 }
