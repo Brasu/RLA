b3edd366e5a9eb50dcc360c045a9abd246e71ed7
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-10 17:05:55 -0300

    Atualizar o exercicio2.js

--- exercicio2.js
+++ exercicio2.js
@@ -1,5 +1,5 @@
 let nome = prompt("Qual seu nome? ");
-let nomeFormatado = nome[0].toUpperCase()+nome.substring(1).toLowerCase()
+let nomeFormatado = nome[0].toUpperCase()+nome.substring(1).toLowerCase();
 let notaAv1 = parseFloat(prompt("Digite sua nota da AV1: "));
 let notaAv2 = parseFloat(prompt("Digite sua nota da AV2: "));
 
@@ -10,10 +10,10 @@ if (notaParcial >= 4){
     let percentualFrequencia = parseFloat(prompt("Qual seu percentual de frequência? "));
     let notaFinal = (notaParcial + notaAv3)/2;
     if (notaFinal >= 5 && percentualFrequencia >= 75) {
-        console.log("Parabéns pela aprovação,", nomeFormatado+"!")
+        console.log("Parabéns pela aprovação,", nomeFormatado+"!");
     } else {
-        console.log("Reprovado na Nota Final.")
+        console.log("Reprovado na Nota Final.");
     }
 } else {
-    console.log("Reprovado na Nota Parcial.")
+    console.log("Reprovado na Nota Parcial.");
 }
