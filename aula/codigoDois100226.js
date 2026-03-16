d70a6070c47f55150391e5f541aae0da2ffa4ac1
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-16 00:21:38 -0300

    Create codigoDois100226.js

---
+++ aula/codigoDois100226.js
@@ -0,0 +1,3 @@
+let name = prompt("Qual seu nome? ");
+let anoNascimento = parseInt(prompt("Que ano você nasceu? "));
+console.log("Oi, meu nome é", name, "e eu tenho", 2026 - anoNascimento, "anos");
