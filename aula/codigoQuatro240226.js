7630c94f04c503b2e195a44e75809bbcd3dd62b4
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-16 00:27:07 -0300

    Create codigoQuatro240226.js

---
+++ aula/codigoQuatro240226.js
@@ -0,0 +1,9 @@
+let num = parseInt(prompt("Digite sua idade: "));
+if (num >= 18) {
+console.log("Parabéns, você é maior de idade!")
+if (num >= 60)
+console.log("Parabéns, você é um idoso!")
+}
+if (num < 18) {
+console.log("Parabéns, você é um menor de idade!")
+}
