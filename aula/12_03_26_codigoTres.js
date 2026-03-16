c5ec635e227efe09ee35936e6578ee7e75893c38
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-16 13:52:57 -0300

    Create 12_03_26_codigoTres.js

---
+++ aula/12_03_26_codigoTres.js
@@ -0,0 +1,17 @@
+// incompleto, precisa ser finalizado.
+let diaSemana = parseInt(prompt("Digite o número da semana: "));
+switch(diaSemana){
+  case 1:
+    console.log("Domingo!");
+    break;
+  case 2:
+    console.log("Segunda-feira!");
+    break;
+  case 3:
+    console.log("Terça-feira!");
+    break;
+  default:
+    console.log("Inválido.")
+}
+    
+      
