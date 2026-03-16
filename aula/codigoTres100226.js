ce59357a59c09f06de37961a5a9ac4857efa7420
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-16 00:22:11 -0300

    Create codigoTres100226.js

---
+++ aula/codigoTres100226.js
@@ -0,0 +1,3 @@
+let num1 = parseInt(prompt("Digite um número: "));
+let num2 = parseInt(prompt("Digite um número: "));
+console.log("Resultado soma:", num1 + num2, "\nResultado subtração:", num1 - num2, "\nResultado divisão:", num1 / num2, "\nResultado multiplicação:", num1 * num2);
