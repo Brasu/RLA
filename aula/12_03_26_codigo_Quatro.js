1178b83f1d9534d4376b4b333d7d0c576584c807
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-16 14:30:50 -0300

    Update 12_03_26_codigo_Quatro.js

--- aula/12_03_26_codigo_Quatro.js
+++ aula/12_03_26_codigo_Quatro.js
@@ -1,32 +1,50 @@
 let tipoDoUsuario = prompt("Qual seu tipo de usuário? ").toLowerCase();  
 let email , senha  
-  
-if(tipoDoUsuario == "admin") {  
-    console.log("-----PAINEL ADMIN-----");  
-    email = prompt("Email: ");  
-    senha = prompt("Senha: ");  
-    if(email == "admin@email" && senha == "admin"){  
-        console.log("Login com sucesso!");  
-    }  
-    else{  
-        console.log("Credenciais inválidas.")  
-    }  
-}  
-else if(tipoDoUsuario == "usuario") {  
-    console.log("-----PAINEL USUÁRIO-----");  
-    email = prompt("Email: ");  
-    senha = prompt("Senha: ");  
-    if(email == "usuario@email" && senha == "senha123") {  
-        console.log("Login com sucesso!");  
-    }  
-    else{  
-        console.log("Credenciais inválidas.")  
-    }  
-}  
-else if(tipoDoUsuario == "convidado") {  
-    console.log("-----PAINEL CONVIDADO-----");  
-    console.log("Login com sucesso!");  
-}  
-else {  
-    console.log("Tipo de usuário inválido!");  
+switch(tipoDoUsuario){
+  case "admin":
+      console.log("-----PAINEL ADMIN-----")
+    email = prompt("Email: ");
+    senha = prompt("Senha: ");
+    switch(email){
+      case "admin@email":
+        switch(senha){
+          case "admin":
+            console.log("Login com sucesso!");
+            break;
+          default:
+            console.log("Senha inválida.");
+        }
+      break;
+      default:
+        console.log("Email inválido.")
+    }
+    break;
+  case "usuario":
+    console.log("-----PAINEL USUÁRIO-----");
+    email = prompt("Email: ");
+    senha = prompt("Senha: ");
+    switch(email){
+      case "usuario@email":
+        switch(senha){
+          case "senha123":
+            console.log("Login com sucesso!");
+            break;
+          default:
+            console.log("Senha inválida.");
+        }
+        break;
+      default:
+        console.log("Email inválido.");
+    }
+    break;
+  case "convidado":
+    console.log("-----PAINEL CONVIDADO-----");
+    console.log("Login com sucesso!");
+    break;
+  default:
+    console.log("Tipo de usuário inválido.");
 }
+            
+   
+
+
