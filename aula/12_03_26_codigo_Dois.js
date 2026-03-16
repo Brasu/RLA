1992fde1c7ad7a4021d1422e2e574bce428ecbea
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-16 13:47:22 -0300

    Create 12_03_26_codigo_Dois.js

---
+++ aula/12_03_26_codigo_Dois.js
@@ -0,0 +1,15 @@
+let mes = parseInt(prompt("Digite o número do mês: "));
+switch(mes){
+  case 1:
+    console.log("Mês Janeiro!");
+    break;
+  case 2:
+    console.log("Mês Fevereiro!!");
+    break;
+  case 3:
+    console.log("Mês Março!");
+    break;
+    default;
+    console.log("Inválido.")
+}
+              
