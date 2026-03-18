8b31e7a02abe5e1cdcf26bd7cdf48dd79eea6926
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-17 21:55:28 -0300

    Create Sistema_de_Tarifas_de_Transporte_Intermunicipal.js

---
+++ trabalho/Sistema_de_Tarifas_de_Transporte_Intermunicipal.js
@@ -0,0 +1,10 @@
+// Sistema de Tarifas de Transporte Intermunicipal
+let nomePassageiro = prompt("Qual o nome do passageiro? ");
+let categoriaPassageiro = prompt("Qual a categoria do passageiro? ");
+let destinoValor = promtp("Qual o destino do passageiro? ");
+let quantidadePassageiros = prompt("Qual a quantidade de passageiros? ");
+let preçoSubtotal = destinoValor * quantidadePassageiros;
+
+switch(categoriaPassageiro){
+  case "adulto":
+    
