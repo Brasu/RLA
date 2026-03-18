7b9cb3ee5b5d20ecac256f2853310153dc7f0f58
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-18 00:07:20 -0300

    Update Sistema_de_Tarifas_de_Transporte_Intermunicipal.js

--- trabalho/Sistema_de_Tarifas_de_Transporte_Intermunicipal.js
+++ trabalho/Sistema_de_Tarifas_de_Transporte_Intermunicipal.js
@@ -1,10 +1,12 @@
-// Sistema de Tarifas de Transporte Intermunicipal
+// Sistema de Tarifas de Transporte Intermunicipal 
 let nomePassageiro = prompt("Qual o nome do passageiro? ");
-let categoriaPassageiro = prompt("Qual a categoria do passageiro? ");
-let destinoValor = prompt("Qual o destino do passageiro? ");
+console.log("Categoria do passageiro (1 - Adulto, 2 - Estudante, 3 - Idoso, 4 - Criança menor de 5 anos)");
+let categoriaPassageiro = parseInt(prompt("Qual a categoria do passageiro? "));
+console.log("Destino (1 - Destino A: R$ 45,00 / 2 - Destino B: R$ 72,00 / 3 - Destino C: R$ 110,00 / 4 - Destino D: R$ 138,00)");
+let destinoValor = parseInt(prompt("Qual o destino do passageiro? "));
 let quantidadePassageiros = prompt("Qual a quantidade de passageiros? ");
 let preçoSubtotal = destinoValor * quantidadePassageiros;
 
 switch(categoriaPassageiro){
-  case "adulto":
+  case 1:
     
