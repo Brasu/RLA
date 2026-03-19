6563bc98f5399c828fecc07b96cbb20bc00f53e7
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-19 17:38:26 -0300

    Update Sistema_de_Tarifas_de_Transporte_Intermunicipal.js

--- trabalho/Sistema_de_Tarifas_de_Transporte_Intermunicipal.js
+++ trabalho/Sistema_de_Tarifas_de_Transporte_Intermunicipal.js
@@ -1,24 +1,27 @@
-// Sistema de Tarifas de Transporte Intermunicipal 
 let nomePassageiro = prompt("Qual o nome do passageiro? ");
-console.log("Categoria do passageiro (1 - Adulto, 2 - Estudante, 3 - Idoso, 4 - Criança menor de 5 anos)");
+console.log("Categoria do passageiro: \n1 - Adulto \n2 - Estudante \n3 - Idoso \n4 - Criança menor de 5 anos");
 let categoriaPassageiro = parseInt(prompt("Qual a categoria do passageiro? "));
-console.log("Destino (1 - Destino A: R$ 45,00 / 2 - Destino B: R$ 72,00 / 3 - Destino C: R$ 110,00 / 4 - Destino D: R$ 138,00)");
+console.log("Opções Destino: \n1 - Destino A \n2 - Destino B \n3 - Destino C \n4 - Destino D");
 let destino = parseInt(prompt("Qual o destino do passageiro? "));
 let quantidadePassageiros = prompt("Qual a quantidade de passageiros? ");
-let desconto, destinoValor;
-
+let desconto, destinoValor, mensagemEmbarque;
+// definições
 switch(categoriaPassageiro){
   case 1:
 desconto = 0
+    tipoDesconto = "Adulto"
     break;
   case 2:
 desconto = 50
+    tipoDesconto = "Estudante"
     break;
   case 3:
 desconto = 50
+    tipoDesconto = "Idoso"
     break;
   case 4:
 desconto = 100
+    tipoDesconto = "Criança"
     break;
   default:
     console.log("Categoria inválida.")
@@ -26,37 +29,43 @@ desconto = 100
 switch(destino){
   case 1:
 destinoValor = 45
+Destino = "Destino A"
     break;
   case 2:
 destinoValor = 72
+Destino = "Destino B"
     break;
   case 3:
 destinoValor = 110
+Destino = "Destino C"
     break;
   case 4:
 destinoValor = 138
+Destino = "Destino D"
     break;
   default:
     console.log("Destino inválido.")
 }
-
+// contas
 let preçoSubtotal = destinoValor * quantidadePassageiros;
 let valorDesconto = preçoSubtotal * (desconto / 100);
 let preçoTotal = preçoSubtotal - valorDesconto;
 
-preçoTotal < 100 ? console.log("Pagamento à vista.") : console.log("Parcelável em até 3X sem juros.");
  switch(true){
-   case preçoTotal(=0):
-     console.log("Embarque gratuito liberado.");
+   case preçoTotal == 0:
+    mensagemEmbarque = "Embarque gratuito liberado.";
      break;
-   case preçoTotal(<100):
-     console.log("Dirija-se ao guichê para pagamento.");
+   case preçoTotal < 100:
+     mensagemEmbarque = "Dirija-se ao guichê para pagamento.";
      break;
-   case preçoTotal(>100 && <200):
-     console.log("Pagamento via terminal disponível.");
+   case preçoTotal < 200:
+     mensagemEmbarque = "Pagamento via terminal disponível.";
      break;
-   case preçoTotal(>=200):
-     console.log("Atendimento priorit´ario no guich^e VIP.");
+   case preçoTotal >= 200:
+     mensagemEmbarque = "Atendimento prioritário no guichê VIP.";
      break;
  }
-console.log("
+ 
+console.log("-----COMPROVANTE-----");
+console.log("Passageiro:", nomePassageiro,"\nQuantidade de passagens:", quantidadePassageiros, "\nDestino:", Destino,"\nEmbarque:", mensagemEmbarque, "\nTipo de desconto:", tipoDesconto, "\nValor total R$:", preçoSubtotal,"\nDesconto:",valorDesconto, "\nValor a pagar R$:",preçoTotal);
+preçoTotal < 100 ? console.log("Pagamento à vista.") : console.log("Parcelável em até 3X sem juros.");
