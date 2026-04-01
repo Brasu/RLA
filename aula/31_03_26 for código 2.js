8d7d9b18e1fe696503f0aeb90c9012afb6f66a91
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-31 22:01:14 -0300

    Update 31_03_26 for código dois.js

--- aula/31_03_26 for código 2.js
+++ aula/31_03_26 for código 2.js
@@ -1,4 +1,8 @@
 let numero = parseInt(prompt("Digite um número: "));
-for(let i = 0; i <= numero; i++){
-    console.log(i)
+let soma = 0
+for(let i = 0; i < numero+1; i++){
+    soma+=i;
+    if(i == numero){
+    console.log(soma)
+    }
 }
