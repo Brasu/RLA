788cc627e35c64ff7d74163e4cde67b93d1bd15a
Brasu <66755169+Brasu@users.noreply.github.com>
2026-03-31 21:39:33 -0300

    Create 31_03_26 código for um.js

---
+++ aula/31_03_26 código for um.js
@@ -0,0 +1,4 @@
+//bloco 1 é executado apenas uma vez. 2 e 3 são executados todas as vezes que o código rodar enquanto forem verdadeiros;
+for(let i = 10; i < 11 && i >= 0; i--){
+    console.log(i)
+}
