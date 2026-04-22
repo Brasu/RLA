7d114069a49d5e92ab3885ccc2f0228d7ec31273
Brasu <66755169+Brasu@users.noreply.github.com>
2026-04-22 14:58:36 -0300

    Update AV2 Caixa Registradora While.js

--- trabalho/AV2 Caixa Registradora While.js
+++ trabalho/AV2 Caixa Registradora While.js
@@ -1,1 +1,21 @@
+let quantidadeProdutos = 0;
+let preçoFinal = 0;
+let maiorPreço = 0;
 
+let preçoProduto;
+
+while(preçoProduto !== -1){
+  
+    quantidadeProdutos++;
+    preçoProduto = parseFloat(prompt("Preço do produto: "));
+    preçoFinal = preçoProduto + preçoFinal;
+  
+ if(preçoProduto > maiorPreço){
+     maiorPreço = preçoProduto
+ }
+    
+}
+ console.log()
+ console.log("Produtos:", quantidadeProdutos);
+ console.log("Total:" , preçoFinal);
+ console.log("Produto mais caro:", maiorPreço);
