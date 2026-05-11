982a50c27ad3d26a7217501134ad19a4d8246492
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-10 22:31:15 -0300

    Update Questao Dois While.js

--- prova salsicha/Questao Dois While.js
+++ prova salsicha/Questao Dois While.js
@@ -1,1 +1,18 @@
+let precoProduto = [];
+let precoFinal = 0;
+let preco;
 
+while(preco !== 0){
+preco = parseFloat(prompt("Preço produto: "));
+  precoProduto.push(preco)
+}
+function somaProdutos(precoFinal, precoProduto){
+ for(let i = 0; i < precoProduto.length; i++){
+   precoFinal += precoProduto[i];
+ }
+}
+if(precoFinal > 100){
+  precoFinal = precoFinal - (precoFinal * 0.1);
+  console.log(precoFinal)
+}
+console.log(precoFinal)
