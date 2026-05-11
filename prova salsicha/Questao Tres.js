810528a9a0402fca8da6a00576535acfbc80a21a
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-11 10:36:25 -0300

    Update Questao Tres.js

--- prova salsicha/Questao Tres.js
+++ prova salsicha/Questao Tres.js
@@ -1,11 +1,17 @@
-const valorBase = parseFloat(prompt("Preço primeiro bilhete: ")):
+const valorBase = parseFloat(prompt("Preço primeiro bilhete: "));
 const numeroBilhete = parseInt(prompt("Número do bilhete desejado: "));
 const valorAumento = parseFloat(prompt("Valor aumento de preço: "));
 
 function calcularPrecoBilhete(valorBase, valorAumento, numeroBilhete){
-  valorBase + (numeroBilhete - 1) * valorAumento;
+ return valorBase + (numeroBilhete - 1) * valorAumento;
 }
+console.log()
 
 calcularPrecoBilhete(valorBase, valorAumento, numeroBilhete);
-console.log(calcularPrecoBilhete(valorBase, valorAumento, numeroBilhete))
-  
+console.log(calcularPrecoBilhete(valorBase, valorAumento, numeroBilhete).toFixed(2));
+
+if(calcularPrecoBilhete(valorBase, valorAumento, numeroBilhete) > 500) {
+  console.log("Lote VIP atingido.");
+} else {
+  console.log("Venda confirmada no lote padrão.");
+}
