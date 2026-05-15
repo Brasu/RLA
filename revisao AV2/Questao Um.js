40cdd37c740e64d4447474ded9afec6a9a0e0b6f
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-14 22:10:31 -0300

    Update Questao Um.js

--- revisao AV2/Questao Um.js
+++ revisao AV2/Questao Um.js
@@ -1,9 +1,9 @@
 let nomeMes, valorGastoMensal;
-let gastoAnual;
+let gastoAnual = 0;
 
 for(let ano = 1; ano >= 1 && ano <= 12; ano++){
   nomeMes = prompt("Mês: ");
-  valorGastoMensal = parseFloat(prompt("Gasto: "));
+  valorGastoMensal = parseFloat(prompt("Gasto: R$ "));
 
   gastoAnual += valorGastoMensal;
 }
