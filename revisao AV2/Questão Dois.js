3f102252d0b24605b4b2c291243721a490cd701e
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-15 10:45:17 -0300

    Update Questão Dois.js

--- revisao AV2/Questão Dois.js
+++ revisao AV2/Questão Dois.js
@@ -1,10 +1,21 @@
-let bilheteTotolec;
+let bilheteTotolec, diferençaMeta;
+let valorTotal = 0;
+
 while(true){
-  bilheteTotolec = parseInt(prompt("
+  bilheteTotolec = parseInt(prompt("Quantidade Totolec: "));
+  if(bilheteTotolec == 0){
+    break;
+  }
+valorTotal += bilheteTotolec;  
 
+}
+console.log()
+console.log("No dia 15/05/2026 foram vendidos", valorTotal, "Totolecs.");
 
-if (valorTotal > 120){
+console.log()
+if (valorTotal >= 120){
   console.log("Você bateu sua meta do dia.");
-} else {
-  console.log("Você não bateu sua meta do dia hoje. Amanhã você deverá vender xx a mais que sua meta diária");
+} else if (valorTotal < 120) {
+  diferençaMeta = 120 - valorTotal
+  console.log("Você não bateu sua meta do dia hoje. Amanhã você deverá vender", diferençaMeta,"a mais que sua meta diária");
 }
