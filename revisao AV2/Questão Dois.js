f527a9aa8460f29fd6e063df99df8a43702d659d
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-14 22:19:04 -0300

    Create Questão Dois.js

---
+++ revisao AV2/Questão Dois.js
@@ -0,0 +1,10 @@
+let bilheteTotolec;
+while(true){
+  bilheteTotolec = parseInt(prompt("
+
+
+if (valorTotal > 120){
+  console.log("Você bateu sua meta do dia.");
+} else {
+  console.log("Você não bateu sua meta do dia hoje. Amanhã você deverá vender xx a mais que sua meta diária");
+}
