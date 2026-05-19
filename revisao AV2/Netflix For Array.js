2020f1bdb944ed4a92df215d59628e302086343b
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-19 20:20:40 -0300

    Atualizar o Netflix For Array.js

--- revisao AV2/Netflix For Array.js
+++ revisao AV2/Netflix For Array.js
@@ -1,5 +1,5 @@
 let quantidadeFilmesAssistidos = parseInt(prompt("Quantos filmes você assistiu esse mês? "));
-let tempoFilme;
+let tempoFilme,mediaFilmes;
 let tempoFilmes = [];
 let filmesLongos = 0;
 let maiorDuraçao = 0;
@@ -7,18 +7,28 @@ let maiorDuraçao = 0;
 for(let contador = 0; contador < quantidadeFilmesAssistidos; contador++){
     tempoFilme = parseInt(prompt("Duração do filme "+(contador+1)+": "));
     tempoFilmes.push(tempoFilme);
+
     if(tempoFilme > 120){
 
     filmesLongos++
  }
+
     if(tempoFilme > maiorDuraçao){
 
     maiorDuraçao = tempoFilme
  }
 
+ let totalDuraçaoFilmes = 0;
+ for(let indiceAtual = 0; indiceAtual < tempoFilmes.length; indiceAtual++){
+   totalDuraçaoFilmes += temposFilmes[indiceAtual];
+ }
+
 }
+
+mediaFilmes = totalDuraçaoFilmes / quantidadeFilmesAssistidos;
+
 console.log();
-console.log("Tempo total assistido:", );
+console.log("Tempo total assistido:", totalDuraçaoFilmes);
 console.log("Filmes longos:", filmesLongos);
-console.log("Duração média:", );
-console.log("Maior duração:", maiorDuração);
+console.log("Duração média:", mediaFilmes.toFixed(2),"minutos");
+console.log("Maior duração:", maiorDuração, "minutos");
