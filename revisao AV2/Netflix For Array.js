3e4349a9721dbd1e41ee4c08cbed393c151f0d85
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-19 19:48:16 -0300

    Atualizar o Netflix For Array.js

--- revisao AV2/Netflix For Array.js
+++ revisao AV2/Netflix For Array.js
@@ -1,3 +1,7 @@
 let quantidadeFilmesAssistidos = parseInt(prompt("Quantos filmes você assistiu esse mês? "));
+let tempoFilme;
 
-for(let contador = 0; contador > quantidadeFilmesAssistidos; contador++){
+for(let contador = 0; contador < quantidadeFilmesAssistidos; contador++){
+    tempoFilme = parseInt(prompt("Duração do filme "+(contador+1)+": "));
+
+}
