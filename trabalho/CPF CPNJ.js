02075201446647f6ad4ff62b889f75d626cf6b56
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-24 23:01:06 -0300

    Create CPF CPNJ.js

---
+++ trabalho/CPF CPNJ.js
@@ -0,0 +1,48 @@
+console.log("Opções de Identificador Estadual: \n
+const identificadorEstadual = parseInt(prompt("Escolha: ");
+let cpf = [];
+
+switch(identificadorEstadual){
+  case 1:
+  cpf[8] = 1;
+    break;
+
+  case 2:
+  cpf[8] = 1;
+    break;
+
+  case 3:
+  cpf[8] = 1;
+    break;
+
+  case 4:
+  cpf[8] = 6;
+    break;
+
+  case 5:
+  cpf[8] = 1;
+    break;
+    
+  case 6:
+  cpf[8] = 1;
+    break;
+    
+  case 7:
+  cpf[8] = 1;
+    break;
+    
+  case 8:
+  cpf[8] = 1;
+    break;
+    
+  case 9:
+  cpf[8] = 1;
+    break;
+    
+  case 0:
+  cpf[8] = 1;
+    break;
+    
+
+    
+    
