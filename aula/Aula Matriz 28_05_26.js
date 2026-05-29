44dbb8faad378c6b1648cb25c124c58aecff7486
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-28 21:30:10 -0300

    Create Aula Matriz 28_05_26 Exemplo Um.js

---
+++ aula/Aula Matriz 28_05_26.js
@@ -0,0 +1,16 @@
+// Matriz usando array
+                    j
+let matriz = [ [1, 2, 3, 4]
+               [5, 6, 7, 8]
+         i     [9, 10, 11, 12]
+               [13, 14, 15, 16] ];
+       i  j 
+matriz[2][1];
+
+/* CRIAR MATRIZ 5X5
+3, 2
+4, 4
+2, 2
+I  J
+Segunda Linha
+*/
