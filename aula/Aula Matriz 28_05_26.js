9235539dedd802ab1c21790c251b51650de59e6f
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-28 21:44:33 -0300

    Update Aula Matriz 28_05_26.js

--- aula/Aula Matriz 28_05_26.js
+++ aula/Aula Matriz 28_05_26.js
@@ -14,3 +14,16 @@ matriz[2][1];
 I  J
 Segunda Linha
 */
+
+let matriz = [[1, 2, 3, 4, 5],
+              [6, 7, 8, 9, 10],
+              [11, 12, 13, 14, 15],
+              [16, 17, 18, 19, 20],
+              [21, 22, 23, 24, 25]];
+
+for(let coluna = 0; coluna < matriz.lenght; coluna++){
+ 
+  for(let linha = 0; linha < matriz.lenght; linha++){
+    console.log(matriz[coluna][linha]
+                }
+}
