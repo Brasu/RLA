c8be68ae2e642e8af2ece106d0eb8d097c3a9907
Brasu <66755169+Brasu@users.noreply.github.com>
2026-05-30 22:48:05 -0300

    Update CPF CPNJ.js

--- trabalho/CPF CPNJ.js
+++ trabalho/CPF CPNJ.js
@@ -1,48 +1,9 @@
-console.log("Opções de Identificador Estadual: \n
-const identificadorEstadual = parseInt(prompt("Escolha: ");
-let cpf = [];
+const nomePessoa = prompt("Nome Completo: ");
+const nomePai = prompt("Nome Completo Pai: ");
+const nomeMae =
 
-switch(identificadorEstadual){
-  case 1:
-  cpf[8] = 1;
-    break;
 
-  case 2:
-  cpf[8] = 1;
-    break;
-
-  case 3:
-  cpf[8] = 1;
-    break;
-
-  case 4:
-  cpf[8] = 6;
-    break;
-
-  case 5:
-  cpf[8] = 1;
-    break;
-    
-  case 6:
-  cpf[8] = 1;
-    break;
-    
-  case 7:
-  cpf[8] = 1;
-    break;
-    
-  case 8:
-  cpf[8] = 1;
-    break;
-    
-  case 9:
-  cpf[8] = 1;
-    break;
-    
-  case 0:
-  cpf[8] = 1;
-    break;
-    
-
-    
-    
+console.log("Registro Geral");
+console.log("Nome", nomePessoa);
+console.log("Filiação");
+console.log("CPF");
