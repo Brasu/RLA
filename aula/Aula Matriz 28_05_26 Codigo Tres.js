776e950ba70e80f186f6df2c45be77c7b3c4bde7
Brasu <66755169+Brasu@users.noreply.github.com>
2026-06-02 19:29:09 -0300

    Atualizar o Aula Matriz 28_05_26 Codigo Tres.js

--- aula/Aula Matriz 28_05_26 Codigo Tres.js
+++ aula/Aula Matriz 28_05_26 Codigo Tres.js
@@ -5,9 +5,32 @@ let matriz = [[1, 2, 3, 4, 5],
               [11, 12, 13, 14, 15],
               [16, 17, 18, 19, 20],
               [21, 22, 23, 24, 25]];
-while(true){
-  let número = parseInt(prompt("Digite um número: "));
-  if(matriz == (número - 1)){
-    console.log("Número existe na matriz!")
-  }
+
+const matriz = [
+    [1, 2],
+    [3, 5],
+];
+
+const search = Number(prompt("Informe um número: "))
+let found
+let candidate
+
+for (let coluna = 0; coluna < matriz.length; coluna++) {
+    for (let linha = 0; linha < matriz.length; linha++) {
+        candidate = matriz[coluna][linha]
+        if (candidate === search) {
+            found = {
+                coluna,
+                linha,
+            }
+        }
+    }
 }
+
+console.log(found)
+
+if (found) {
+    console.log("O seu número foi localizado na coluna", found.coluna, "e linha", found.linha)
+    return
+}
+console.log("Seu número não foi encontrado")
